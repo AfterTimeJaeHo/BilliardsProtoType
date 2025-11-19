e4059642e6b11b15e99fb4f01d074bb6444566c8
JAEHO CHOI <chlwogh045@aftertime.co.kr>
2025-11-19 10:11:42 +0900

    add: 공통 스크립트

---
+++ Assets/Scripts/Common/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace Aftertime.SecretSome.Common
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static event Action onApplicationPause = delegate { };
+        public static event Action onApplicationQuit = delegate { };
+        
+        [SerializeField] private GameObject border;
+
+        private void Awake()
+        {
+            if (border != null)
+                border.SetActive(true);
+
+#if UNITY_ANDROID
+            Application.targetFrameRate = 60;
+
+#endif
+        }
+
+
+        public static float GameSpeed
+        {
+            get => Time.timeScale;
+            set => Time.timeScale = value;
+        }
+
+        private void OnApplicationPause(bool pauseStatus)
+        {
+            onApplicationPause();
+        }
+
+        private void OnApplicationQuit()
+        {
+            onApplicationQuit();
+        }
+    }
+}
---
+++ Assets/Scripts/Common/ResourcesPath.cs
@@ -0,0 +1,12 @@
+﻿namespace Aftertime.SecretSome.Common
+{
+    public class ResourcesPath
+    {
+        public const string BGPrefix = "Textures/BG/";
+        public const string ECGPrefix = "Textures/ECG/";
+        public const string SCGPrefix = "Textures/SCG/";
+        public const string StandingPrefix = "Textures/Standing/";
+        public const string FacePrefix = "Textures/Face/";
+        public const string StreamingChatProfilePrefix = "Textures/StreamingChatProfile/";
+    }
+}
